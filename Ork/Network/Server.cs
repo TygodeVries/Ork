98b42eb904fdab5edd0427c287bff23539cc2036
Tygo de Vries <zttdevries@gmail.com>
2026-04-11 00:43:43 +0200

    new

--- Ork/Network/Server.cs
+++ Ork/Network/Server.cs
@@ -47,8 +47,9 @@ namespace Ork.Network
             if (pending)
             {
                 TcpClient network = listener.AcceptTcpClient();
-                Console.WriteLine($"Someone Connected From {network.Client.AddressFamily}!");
-                File.WriteAllText($"{DateTime.Now.Ticks}.txt", $"{network.Client.AddressFamily}");
+                Console.WriteLine($"Someone Connected From {network.Client.RemoteEndPoint}!");
+
+                File.WriteAllText($"{DateTime.Now.Ticks}.txt", $"{network.Client.RemoteEndPoint}");
                 Connection connection = new Connection(network);
                 AcceptNewClient?.Invoke(connection);
                 connections.Add(connection);
